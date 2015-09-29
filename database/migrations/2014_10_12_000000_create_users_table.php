fbfff370d053fe367cd9f18c442688aad3e0cb0a
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2015-09-29 07:07:39 +0000

    Migrated Users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('handle');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('motto', 30)->nullable();
+            $table->string('nymi', 60)->nullable();
+            $table->string('sponsorid, 60');
+            $table->string('photoPath, 60');
+            //$table->integer('v3c', 18);
+            //$table->integer('ninja', 18)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
