48c1c38e3aeeefdf129201e08d967ccb9d558ef1
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2015-10-14 15:58:35 +0000

    Added home pages text buttons

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,11 +9,15 @@
 @section('leftProfile')
 <h1>Amaricus</h1>
 
-<a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">123 234 342 233</button></a>
+<a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">100</button></a>
+<a href="{{ url('/auth/nymi') }}"><button type = "button" class = "navButton">1000</button></a>
+
+
 <p>This is someone's motto, let's see how long it can be
-Should be at least 3 lines long by default. Right?  One more line</p>
+Should be at least 3 lines long by default. Right?</p>
     <hr/>
 <h2>Top 3</h2>
+
 <ul>
     <li><a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">Testimony of a Toker</button></a></li>
     <li><a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">Destiny of Forgiveness</button></a></li>
@@ -27,13 +31,12 @@ Should be at least 3 lines long by default. Right?  One more line</p>
 @stop
 @section('centerMenu')
     <h2>This is a title</h2>
-    <p>Some things in CSS are a bit tedious to write,
-        especially with CSS3 and the many vendor prefixes
-        that exist. A mixin lets you make groups of CSS
-        declarations that you want to reuse throughout your
-        site. You can even pass in values to make your mixin
-        more flexible. A good use of a mixin is for vendor
-        prefixes. Here's an example for border-radius.</p>
+    <a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">Elevate: 133 123 120</button></a>
+    <a href="{{ url('/auth/nymi') }}"><button type = "button" class = "navButton">Extend: 103 423 4</button></a>
+ <table align = "center" border = "1" style = "margin: 23px auto;">
+     <tr><td>Indexer</td><td>Date</td><td>Belief Center</td></tr>
+     <tr><td>#Bab</td><td>9/23/2014</td><td>Treacy Levine Center </td></tr>
+ </table>
 @stop
 @section('centerText')
     <hr class = "mainHr">
@@ -51,6 +54,27 @@ Should be at least 3 lines long by default. Right?  One more line</p>
         site. You can even pass in values to make your mixin
         more flexible. A good use of a mixin is for vendor
         prefixes. Here's an example for border-radius.</p>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+    </p>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+    </p>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+    </p>
 
 
 
@@ -63,10 +87,6 @@ Should be at least 3 lines long by default. Right?  One more line</p>
 @stop
 @section('rightProfile')
     <h2>Inspired By:</h2>
-    <select>
-        <option value="Handle">Handle</option>
-        <option value="Post">Post</option>
-    </select>
     <ul>
         <li><a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">1Dr. Greenthumb</button></a></li>
         <li><a href="{{ url('/auth/nymi') }}"><button type = "button" class = "interactButton">Majestic</button></a></li>
