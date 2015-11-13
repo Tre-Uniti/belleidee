dca38210cb253719f99187ff7b92fb591de9ca9f
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2015-11-13 03:47:47 +0000

    Pw Reset updates

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,59 +1,35 @@
 @extends('auth')
 
 @section('login')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Reset Password</div>
-                    <div class="panel-body">
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+    <h3>Reset Password</h3>
+    <form role="form" method="POST" action="{{ url('/password/reset') }}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="token" value="{{ $token }}">
 
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="hidden" name="token" value="{{ $token }}">
+        <table align = "center">
+            <tr>
+                <td><label for = "email" class = "login">E-Mail Address</label></td>
+            </tr>
+            <tr>
+                <td><input type= "email" id = "email" class = "welcomeInputText" name="email" value="{{ old('email') }}"></td>
+            </tr>
+            <tr>
+                <td><label for = "password" class = "login">Password</label></td>
+            </tr>
+            <tr>
+                <td><input type="password" id = "password" class = "welcomeInputText" name="password"></td>
+            </tr>
+            <tr>
+                <td><label for = "password_confirmation" class = "login">Confirm Password</label></td>
+            </tr>
+            <tr>
+                <td><input type="password" id = "password_confirmation" class = "welcomeInputText" name="password_confirmation"></td>
+            </tr>
+        </table>
+        <button type="submit" class="navButton">Reset Password</button>
+    </form>
+@stop
+@section('footer')
+    <h5>You may email Zoko@belle-idee.org if login problems persist.</h5>
+@stop
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Confirm Password</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password_confirmation">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Reset Password
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
