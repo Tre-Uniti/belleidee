4fa55516833cf1e8ff43466078b6d2810e9b173a
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2015-11-17 02:55:39 +0000

    proxyFix

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -37,7 +37,7 @@ return [
     'headers' => [
         \Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
         \Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
-        \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_SCHEME',
+        \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
         \Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
     ]
 ];
