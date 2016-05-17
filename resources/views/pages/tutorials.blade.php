c42c45c50d0c9dead62858d269d5d53ea61611ff
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2016-05-17 07:01:54 +0000

    Embedded Introduction and Draft videos

--- resources/views/pages/tutorials.blade.php
+++ resources/views/pages/tutorials.blade.php
@@ -5,21 +5,21 @@
 @section('centerText')
     <h2>Idee Tutorials</h2>
 
-    <h4>Starter:</h4>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/b3ZHp36LaRk" frameborder="0" allowfullscreen></iframe>
+    <h4>Introduction:</h4>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/SiPMUbMttwg" frameborder="0" allowfullscreen></iframe>
     <!--//How to create a draft and convert
     //How to extend a post and view extensions
     //How to elevate and view elevations (+rankings)
     //How to sort posts/extensions and your home
     //Platform support (mobile, tablet, computer)
     -->
-    <h4>Intermediate:</h4>
+    <h4>Drafts:</h4>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/In8p0IJJNTU" frameborder="0" allowfullscreen></iframe>
     <!--Change user settings, types of users (user, mod, admin)
     //How to localize a post or extension to a beacon
     //Start a sponsorship, benefits of sponsorship
     //Bookmarking posts, extensions, users
     //How to do searching (local vs global)-->
-    <h4>Advanced:</h4>
     <!--//Community question and answers/extensions
     //Legacy extension and users
     //Intolerance reporting
