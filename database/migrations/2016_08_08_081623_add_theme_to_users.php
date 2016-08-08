44a3363dddaeb0dd454ab9e1748890e1041ee836
Brendan McGoffin <brendan.mcgoffin@tre-uniti.org>
2016-08-08 08:17:21 +0000

    Updated migration name

--- database/migrations/2016_08_08_081623_add_theme_to_users.php
+++ database/migrations/2016_08_08_081623_add_theme_to_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddThemesToUsers extends Migration
+class AddThemeToUsers extends Migration
 {
     /**
      * Run the migrations.
